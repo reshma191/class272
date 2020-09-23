b992e35e50b9c4ef416cc1620dceeff4bfafd4f8
reshma <reshma.bdeepak@gmail.com>
2020-09-23 18:59:04 +0800

    Add chain

--- sketch.js
+++ sketch.js
@@ -28,11 +28,11 @@ function setup(){
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
 
-    log3 =  new Log(810,180,300, PI/2);
+    log3 =  new Log(100,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    log5 = new Log(100,120,150, -PI/7);
 
     bird = new Bird(100,100);
 
